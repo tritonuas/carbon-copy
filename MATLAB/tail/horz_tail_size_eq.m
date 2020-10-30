51a1c3a9f0e61f58ca00ea45e2a2bf6d884a98c7
Allyson Chen <aechen@ucsd.edu>
2020-10-30 10:53:08 -0700

    adds tail files

---
+++ MATLAB/tail/horz_tail_size_eq.m
@@ -0,0 +1,22 @@
+function [x_ach, x_cg, S_h, c, S, C_HT] = horz_tail_size_eq(x_ach, x_cg, S_h, c, S, C_HT, hasx_ach, hasx_cg, hasS_h, hasc, hasS,...
+    hasC_HT)
+
+if ~hasx_ach
+    x_ach = C_HT*(c*S)/S_h+x_cg;
+
+elseif ~hasx_cg
+    x_cg = -C_HT*(c*S)/S_h+x_ach;
+    
+elseif ~hasS_h
+    S_h = C_HT*(c*S)/(x_ach-x_cg);
+    
+elseif ~hasc
+    c = (x_ach - x_cg)*S_h/(S*C_HT);
+
+elseif ~hasS
+    S = (x_ach - x_cg)*S_h/(c*C_HT);
+    
+elseif ~hasC_HT
+    C_HT = (x_ach-x_cg)*S_h/(c*S);
+    
+end
---
+++ MATLAB/tail/vert_tail_size_eq.m
@@ -0,0 +1,30 @@
+ 
+
+function [wingspan, S_v, wingarea, x_acv, x_cg, C_VT] = vert_tail_size_eq(x_acv, x_cg, S_v, wingspan, wingarea, C_VT, hasx_acv, hasx_cg, hasS_v, hasb, hasS,...
+    hasC_VT)
+
+if ~hasx_acv
+    x_acv = C_VT*wingspan*wingarea/S_v+x_cg;
+    
+elseif ~hasx_cg
+    x_cg = -C_VT*wingspan*wingarea/S_v+x_acv;
+    
+elseif ~hasS_v
+    S_v = C_VT*wingspan*wingarea/(x_acv-x_cg);
+    
+elseif ~hasb
+    wingspan = (x_acv-x_cg)*S_v/C_VT*wingarea;
+    
+elseif ~hasS
+    wingarea = (x_acv-x_cg)*S_v/C_VT*wingspan;
+    
+elseif ~hasC_VT
+    C_VT = (x_acv-x_cg)*S_v/wingarea*wingspan;
+   
+end
+    
+
+
+
+
+
