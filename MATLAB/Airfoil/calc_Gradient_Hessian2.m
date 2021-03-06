2a63089ba03ecf373a6d1ca331c43719513333c1
Erik Lovekin <elovekin@ucsd.edu>
2021-03-06 12:41:52 -0800

    calc_Gradient_Hessian2 created to be more efficient than calc_Gradient_Hessian by not storing so many values multiple times

--- MATLAB/Airfoil/calc_Gradient_Hessian2.m
+++ MATLAB/Airfoil/calc_Gradient_Hessian2.m
@@ -1,9 +1,10 @@
-function [Gradient, Hessian] = calc_Gradient_Hessian(Cl, A, B, CC, directory)  
-    Airfoils = cell(2,4,3);
-    negclcd = cell(2,4,3);
-    frstprtialderderivative = cell(1,4,3);
+function [Gradient, Hessian] = calc_Gradient_Hessian2(Cl, A, B, CC, directory)  
+    Airfoils = cell(4,4);
+    negclcd = cell(4,4);
+    frstprtialderderivative = cell(3,4);
     Gradient = zeros(3,1);
     Hessian = zeros(3);
+    Airfoils{1,1} = [num2str(A), num2str(B),num2str(CC)];
     for v = 1:3 % three dimensions for debugging, 3rd index for arrays not necessary
                 %1 ==> A; 2 ==> B; 3 ==> CC
         r = 0;
@@ -29,22 +30,22 @@ function [Gradient, Hessian] = calc_Gradient_Hessian(Cl, A, B, CC, directory)
             elseif v == 3
                 cc = i;
             end    
-            Airfoils{r,1,v} = [num2str(a), num2str(b),num2str(cc)];
-            Airfoils{r,2,v} = [num2str(a+1), num2str(b), num2str(cc)];
-            Airfoils{r,3,v} = [num2str(a), num2str(b+1), num2str(cc)];
-            Airfoils{r,4,v} = [num2str(a), num2str(b), num2str(cc+1)];
+            
+            Airfoils{r,2} = [num2str(a+1), num2str(b), num2str(cc)];
+            Airfoils{r,3} = [num2str(a), num2str(b+1), num2str(cc)];
+            Airfoils{r,4} = [num2str(a), num2str(b), num2str(cc+1)];
                  disp(Airfoils); %TEMPORARY FOR TESTING
             for c = 1:4
-                negclcd{r,c,v} = findclcd(Cl, Airfoils{r,c,v}, directory);
+                negclcd{r,c} = findclcd(Cl, Airfoils{r,c}, directory);
             end
         end
         for c = 1:4
-            frstprtialderderivative{1,c,v} = negclcd{2,c,v} - negclcd{1,c,v};
+            frstprtialderderivative{1,c} = negclcd{2,c} - negclcd{1,c};
         end
         
-        Gradient(v,1) = frstprtialderderivative{1,1,v};
+        Gradient(v,1) = frstprtialderderivative{1,1};
         for clm = 2:4
-            Hessian(v,clm-1) = frstprtialderderivative{1,clm,v} - frstprtialderderivative{1,1,v}; %Hessian needs some work
+            Hessian(v,clm-1) = frstprtialderderivative{1,clm} - frstprtialderderivative{1,1}; %Hessian needs some work
         end
     end
      disp(Gradient);disp(Hessian);%TEMPORARY FOR TESTING
