76c14c7ed8bda5696eadcbe21df40eeeac2e7c66
Allyson Chen <aechen@ucsd.edu>
2021-08-28 14:22:32 -0700

    added weight consideration into main script
    	modified:   MATLAB/MainScriptExe.m
    	new file:   MATLAB/MainScriptExe.m~
    	modified:   MATLAB/Weight/compute_weight_analytic.m
    	modified:   MATLAB/Weight/weight_fuse_analytic.m
    	modified:   MATLAB/Weight/weight_wing_analytic.m
    	new file:   MATLAB/Weight/weight_wing_analytic.m~

--- MATLAB/Weight/weight_fuse_analytic.m
+++ MATLAB/Weight/weight_fuse_analytic.m
@@ -20,6 +20,6 @@ function w_fuse = weight_fuse_analytic(density_carbon_epoxy, num_plies, num_bulk
     bulkhead_area_density = density_plywood*num_bulkheads*area_fraction_bulkhead*t_bulkhead; % kg/m^2   
     carbon_epoxy_area_density = density_carbon_epoxy*num_plies;
     
-    w_fuse = carbon_epoxy_area_density(2*fuse_length*fuse_width+2*fuse_length*fuse_height+2*fuse_height*fuse_width) + fuse_height*fuse_width*bulkhead_area_density;
+    w_fuse = carbon_epoxy_area_density*(2*fuse_length*fuse_width+2*fuse_length*fuse_height+2*fuse_height*fuse_width) + fuse_height*fuse_width*bulkhead_area_density;
     w_fuse = fudgefactor*w_fuse;
 end
