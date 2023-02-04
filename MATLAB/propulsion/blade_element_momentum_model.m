3196dfa2982f067eb31bf8f360879c43ec88cba0
Caden Zhou <roz003@ucsd.edu>
2023-02-03 17:36:06 -0800

    Fixed the phi calc and added Cl changes vector

--- MATLAB/propulsion/blade_element_momentum_model.m
+++ MATLAB/propulsion/blade_element_momentum_model.m
@@ -17,18 +17,20 @@ clear all;
     
     Cl=0.75;
     CD=0.5;
-    chord=0.05;
+    chord=0.05; %% Add chord distribution next
     air_density=1.225;
     section_radius=linspace(0,radius,numsection);
     
-    for i=2:numsection
+    for i=1:numsection
     V(i)=sqrt((section_radius(i)*rotation_velocity)^2+plane_velocity^2);
-    phi(i)=angle(i)-atand(plane_velocity/(section_radius(i)*rotation_velocity));
+    phi(i)=atan2d(plane_velocity, rotation_velocity*section_radius(i)); %Fixed phi calculations.. it was calculating alpha before
+    alpha(i)=(angle(i)-phi(i))*pi/180; %Gets the Alpha
+    Cl(i)=(2*pi)*alpha(i)*0.8;
     end
     
     dr=radius/numsection;
     
-    Lift=Cl*0.5*air_density.*(V).^2*chord*dr;
+    Lift=Cl(i).*0.5*air_density.*(V).^2*chord*dr;
     Drag=CD*0.5*air_density.*(V).^2*chord*dr;
     
     Thrust=num_blades.*sum(Lift.*cosd(phi)-Drag.*sind(phi));
